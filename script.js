2ff36f3707f11504f409d6d04dbeebbd1fb7070f
Rafael Caballero <67808421+Ginazai@users.noreply.github.com>
2023-07-18 07:55:25 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	var blur_count = 0;
 
-	$('.list-group-item')click(function() {
+	$('.list-group-item').click(function() {
 		$('.list-group-item').removeClass('active');
 		$(this).toggleClass('active');
 		blur_count = 0;
